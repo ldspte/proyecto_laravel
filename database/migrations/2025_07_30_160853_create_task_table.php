d62cf59d92ac37fa2f7248ca3d8dfd7fb1acd0ac
ldspte <ldspte9807@gmail.com>
2025-07-30 11:19:16 -0500

    tablas

---
+++ database/migrations/2025_07_30_160853_create_task_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->string('tittle');
+            $table->string('description');
+            $table->string('status');
+            $table->string('priority');
+            $table->string('estimated_hours');
+            $table->string('actual_hours');
+            $table->string('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+};
